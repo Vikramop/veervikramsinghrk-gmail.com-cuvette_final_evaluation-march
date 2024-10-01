d2be5d7464f87ac848050d301b69bf50d27cbf04
Vikramop <veervikramsingh132@gmail.com>
2024-10-01 22:32:46 +0530

    sm deply changes

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,11 @@ import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.routes.js';
 import storyRoutes from './routes/story.routes.js';
 import cors from 'cors';
-import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-// console.log('__dirname', __dirname);
 
 app.use(
   cors({
@@ -24,14 +20,6 @@ app.use(express.json()); // middleware, parses req.body
 
 app.use('/api/auth', authRoutes);
 app.use('/api/story', storyRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  app.length('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
-}
 
 app.listen(PORT, () => {
   connectDB();
